c28543d4471cf291012e1fe21979dce2e82b0997
Kim Nayeon <87288958+kimnayeon16@users.noreply.github.com>
2024-02-08 17:11:53 +0900

    [level 0] Title: 이진수 더하기, Time: 0.04 ms, Memory: 60.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.kt
@@ -0,0 +1,6 @@
+class Solution {
+    fun solution(bin1: String, bin2: String): String {
+        
+        return Integer.toBinaryString(Integer.parseInt(bin1, 2) + Integer.parseInt(bin2, 2))
+    }
+}
