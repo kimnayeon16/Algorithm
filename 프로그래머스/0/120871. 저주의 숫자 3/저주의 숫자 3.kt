013f4d3fb364ba1bc6200b3a0793890e1169afdf
Kim Nayeon <87288958+kimnayeon16@users.noreply.github.com>
2024-02-14 09:46:03 +0900

    [level 0] Title: 저주의 숫자 3, Time: 10.50 ms, Memory: 60.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun solution(n: Int): Int {
+        var answer: Int = 0
+        
+        for(i in 1..n){
+            answer++
+            while(answer % 3 == 0 || answer.toString().contains("3")){
+                answer += 1
+            }
+        }
+        return answer
+    }
+}
