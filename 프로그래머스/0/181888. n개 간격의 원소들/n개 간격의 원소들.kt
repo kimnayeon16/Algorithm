17f5165b7759e0ba173b0ede109e761f925911d2
Kim Nayeon <87288958+kimnayeon16@users.noreply.github.com>
2024-02-18 00:00:09 +0900

    [level 0] Title: n개 간격의 원소들, Time: 12.79 ms, Memory: 63 MB -BaekjoonHub

---
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(num_list: IntArray, n: Int): IntArray {
+        var answer: IntArray = intArrayOf()
+        for(i in 0 until num_list.size step n){
+            answer += num_list[i]
+        }
+        return answer
+    }
+}
