863fec9c9163ab5e2b671f432f89c43ea7188b06
Kim Nayeon <87288958+kimnayeon16@users.noreply.github.com>
2024-02-20 00:48:44 +0900

    [level 0] Title: 부분 문자열인지 확인하기, Time: 7.44 ms, Memory: 61.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(my_string: String, target: String): Int {
+        if(my_string.contains(target)){
+            return 1
+        }else{
+            return 0
+        }
+    }
+}
