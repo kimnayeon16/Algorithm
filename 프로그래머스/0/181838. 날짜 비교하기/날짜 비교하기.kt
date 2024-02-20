9ef89c489e5d6da25fb70e180896a06536d70a23
Kim Nayeon <87288958+kimnayeon16@users.noreply.github.com>
2024-02-20 10:03:46 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.01 ms, Memory: 62.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun solution(date1: IntArray, date2: IntArray): Int {
+        
+        if(date1[0] < date2[0]){
+            return 1
+        }else if(date1[0] > date2[0]){
+            return 0
+        }else if(date1[1] < date2[1]){
+            return 1
+        }else if(date1[1] > date2[1]){
+            return 0
+        }else if(date1[2] < date2[2]){
+            return 1
+        }else if(date1[2] > date2[2]){
+            return 0
+        }else{
+            return 0
+        }
+    }
+}
