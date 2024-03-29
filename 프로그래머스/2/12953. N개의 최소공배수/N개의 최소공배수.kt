0843bf929dc68bdefd221d32ac9bac9ee28c0401
Kim Nayeon <87288958+kimnayeon16@users.noreply.github.com>
2024-03-29 15:50:18 +0900

    [level 2] Title: N개의 최소공배수, Time: 37.96 ms, Memory: 62.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.kt
@@ -0,0 +1,24 @@
+class Solution {
+    fun solution(arr: IntArray): Int {
+        var answer = 0
+        var arrSorted = arr.sorted()
+        var max = arrSorted[arrSorted.size-1]
+        println(max)
+        
+        var cnt = 0
+        var num = 1
+        while(cnt != arrSorted.size-1){
+            // println("비교 값 ${max*num}")
+            cnt = 0
+            for(i in 0 until arrSorted.size-1){
+                if((num * max) % arrSorted[i] == 0){
+                    cnt++
+                }
+            }
+            // println(cnt)
+            num++
+        }
+        answer = (num-1) * max
+        return answer
+    }
+}
