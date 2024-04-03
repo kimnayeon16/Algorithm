3db02d4a46ed48c96406798b68ae7e8342da4d44
Kim Nayeon <87288958+kimnayeon16@users.noreply.github.com>
2024-04-03 09:54:31 +0900

    [level 1] Title: 덧칠하기, Time: 21.81 ms, Memory: 65.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.kt
@@ -0,0 +1,21 @@
+class Solution {
+    fun solution(n: Int, m: Int, section: IntArray): Int {
+        var answer: Int = 0
+        
+        var sortedSec = section.sorted()
+        var start = section[0]
+        var idx = 1
+        answer++
+        
+        while(idx != section.size){
+            if(sortedSec[idx] - start < m){
+                // continue
+            }else{
+                start = sortedSec[idx]
+                answer++
+            }
+            idx++
+        }
+        return answer
+    }
+}
