1a07181e47d901e86b456f0ce9632834020f91d1
Kim Nayeon <87288958+kimnayeon16@users.noreply.github.com>
2024-05-29 15:50:28 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 30.03 ms, Memory: 64.7 MB -BaekjoonHub

--- 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.kt
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.kt
@@ -1,10 +1,8 @@
 class Solution {
     fun solution(arr: IntArray, k: Int): IntArray {
         var answer = IntArray(k, {-1})
-        var sortedArr = arr.sorted()
         
         var idx = 0
-        
         arr.forEach {
             if(idx < k && !answer.contains(it)){
                 answer[idx++] = it
