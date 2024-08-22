aadf7f305bd18a4ca41800ed868ddf5a8d3a6d1e
Kim Nayeon <87288958+kimnayeon16@users.noreply.github.com>
2024-08-22 14:40:55 +0900

    [Silver I] Title: 회전 초밥, Time: 348 ms, Memory: 42336 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.java
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.java
@@ -1,45 +1,55 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		ArrayList<Integer> bob = new ArrayList<>();
 		
 		int N = sc.nextInt(); //접시의 수
 		int d = sc.nextInt(); //초밥의 가짓수
 		int k = sc.nextInt(); //연속해서 먹는 접시의 수
 		int c = sc.nextInt(); //쿠폰 번호
-		int result = 0;
+		
+		int[] sushiBelt = new int[N];
+		int[] sushi = new int[d+1];
+		int count = 0;
+		int max = 0;
 		
 		//배열리스트에 회전 초밥 벨트에 놓인 접시 번호 추가하기
 		for(int i=0; i<N; i++) {
-			bob.add(sc.nextInt());
+			sushiBelt[i] = sc.nextInt();
 		}
 		
-		
-		for(int i=0; i<N; i++) {
-			int[] count = new int[d+1];
-			int bobCount = 0;
-			
-			//벨트에 초밥이 있다면 +1
-			for(int j=0; j<k; j++) {
-				count[bob.get((i+j) % N)]++;
+		//처음 최소 접시 k개에 대해 체크
+		for(int j=0; j<k; j++) {
+			if(sushi[sushiBelt[j]] == 0) {
+				count++;
 			}
-			
-			//초밥 쿠폰
-			count[c]++;
-			
-			//만약 0이 아니라면 초밥이 있는 것이므로 카운트 증가
-			for(int a=1; a<=d; a++) {
-				if(count[a] != 0) {
-					bobCount++;
+			sushi[sushiBelt[j]]++;
+		}
+		max = count;
+		
+		
+		for(int n=0; n<N; n++) {
+			//count의 값이 커졌다는 것은 초밥의 종류가 변경되었다는 것.
+			//쿠폰 접시가 빠진거라면 count에 추가해줘야함.
+			if(count >= max) {
+				if(sushi[c] == 0) {
+					max = count+1;
+				}else {
+					max = count;
 				}
 			}
-			//최댓값 비교
-			result = Math.max(result, bobCount);
+			
+			//k개의 접시 중 첫번째 접시 빼기
+			sushi[sushiBelt[n]]--;
+			if(sushi[sushiBelt[n]] == 0) count--;
+			
+			//하나 뺏으니까 접시 하나 넣기
+			if(sushi[sushiBelt[(n + k) % N]] == 0) count++;
+			sushi[sushiBelt[(n + k) % N]]++;
 		}
 		//출력
-		System.out.println(result);
+		System.out.println(max);
 	}
 }
