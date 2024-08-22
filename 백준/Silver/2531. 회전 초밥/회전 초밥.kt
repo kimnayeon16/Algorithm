823190147c1f73f1f28b2e41e3de54c6459a179e
Kim Nayeon <87288958+kimnayeon16@users.noreply.github.com>
2024-08-22 14:51:08 +0900

    [Silver I] Title: 회전 초밥, Time: 248 ms, Memory: 25736 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.kt
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.kt
@@ -1,29 +1,37 @@
 fun main(){
     var(N, d, k, c) = readln().split(" ").map { it.toInt() }
-    var list = mutableListOf<Int>()
-    var result = 0
+    var sushiBelt = MutableList(N){0}
+    var sushi = MutableList(d + 1){0}
 
-    repeat(N){
-        list.add(readln().toInt())
-    }
+    var cnt = 0
 
     for(i in 0 until N){
-        var count = MutableList(d+1){0}
-        var bobCount = 0
-
-        for(j in 0 until k){
-            count[list[(i+j) % N]]++
-        }
-        count[c]++
-
-        for(a in 1..d){
-            if(count[a] != 0){
-                bobCount++
-            }
-        }
-        result = maxOf(result, bobCount)
-
+        sushiBelt[i] = readln().toInt()
     }
 
-    println(result)
+    //최초의 k개의 벨트에 대해서 접시 확인
+    for(j in 0 until k){
+        if(sushi[sushiBelt[j]] == 0) cnt++
+        sushi[sushiBelt[j]]++
+    }
+    var max = cnt
+
+    for(n in 0 until N){
+        if(max <= cnt){
+            if(sushi[c] == 0){
+                max = cnt+1
+            }else{
+                max = cnt
+            }
+        }
+
+        //첫번째 접시 빼기
+        sushi[sushiBelt[n]]--
+        if(sushi[sushiBelt[n]] == 0) cnt--
+
+        //접시 추가하기
+        if(sushi[sushiBelt[(n + k) % N]] == 0) cnt++
+        sushi[sushiBelt[(n + k) % N]]++
+    }
+    println(max)
 }
