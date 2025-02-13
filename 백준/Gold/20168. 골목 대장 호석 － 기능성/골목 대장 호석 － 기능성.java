2a8516df378479d6bcb052e21a2f3a589673a546
Kim Nayeon <87288958+kimnayeon16@users.noreply.github.com>
2025-02-13 15:52:08 +0900

    [Gold V] Title: 골목 대장 호석 - 기능성, Time: 64 ms, Memory: 11736 KB, Score: 8 point -BaekjoonHub

---
+++ 백준/Gold/20168. 골목 대장 호석 － 기능성/골목 대장 호석 － 기능성.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static class Node implements Comparable<Node>{
+		private int idx;
+		private int cost;
+		
+		public Node(int idx, int cost) {
+			this.idx = idx;
+			this.cost = cost;
+		}
+		
+		public int compareTo(Node o) {
+			return o.cost - this.cost;
+		}
+	}
+	static int N;
+	static ArrayList<ArrayList<Node>> graph = new ArrayList<ArrayList<Node>>();
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		
+		N = Integer.parseInt(st.nextToken()); //교차로 개수
+		int M = Integer.parseInt(st.nextToken()); //골목 개수
+		int A = Integer.parseInt(st.nextToken()); //시작
+		int B = Integer.parseInt(st.nextToken()); //끝
+		int C = Integer.parseInt(st.nextToken()); //가진 돈
+		
+		for(int i=0; i<=N; i++) {
+			graph.add(new ArrayList<>());
+		}
+		
+		for(int i=0; i<M; i++) {
+			st = new StringTokenizer(bf.readLine());
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			int cost = Integer.parseInt(st.nextToken());
+			
+			graph.get(from).add(new Node(to, cost));
+		}
+		
+		System.out.println(solve(A, B, C));
+	}
+	
+	public static int solve(int start, int end, int goal) {
+		int[] dist = new int[N+1];
+		int max = Integer.MIN_VALUE;
+		
+		PriorityQueue<Node> queue = new PriorityQueue<Node>();
+		queue.add(new Node(start, 0));
+		
+		while(!queue.isEmpty()) {
+			Node tmp = queue.poll();
+			
+			if(tmp.idx == end && tmp.cost <= goal) {
+				return max;
+			}
+			
+			for(Node next: graph.get(tmp.idx)) {
+				if(next.cost + tmp.cost <= goal) {
+					if(dist[next.idx] < next.cost + tmp.cost) {
+						dist[next.idx] = next.cost + tmp.cost;
+						queue.add(new Node(next.idx, dist[next.idx]));
+						max = Math.max(max, next.cost);
+					}
+				}
+			}
+		}
+		return -1;
+	}
+}
