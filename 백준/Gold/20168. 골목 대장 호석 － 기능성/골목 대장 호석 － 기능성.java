a264aa6020de453a2684ca67c59f4b12da689240
Kim Nayeon <87288958+kimnayeon16@users.noreply.github.com>
2025-02-13 16:05:11 +0900

    [Gold V] Title: 골목 대장 호석 - 기능성, Time: 68 ms, Memory: 11736 KB, Score: 9 point -BaekjoonHub

--- 백준/Gold/20168. 골목 대장 호석 － 기능성/골목 대장 호석 － 기능성.java
+++ 백준/Gold/20168. 골목 대장 호석 － 기능성/골목 대장 호석 － 기능성.java
@@ -10,10 +10,12 @@ public class Main {
 	public static class Node implements Comparable<Node>{
 		private int idx;
 		private int cost;
+		private int max;
 		
-		public Node(int idx, int cost) {
+		public Node(int idx, int cost, int max) {
 			this.idx = idx;
 			this.cost = cost;
+			this.max = max;
 		}
 		
 		public int compareTo(Node o) {
@@ -43,7 +45,8 @@ public class Main {
 			int to = Integer.parseInt(st.nextToken());
 			int cost = Integer.parseInt(st.nextToken());
 			
-			graph.get(from).add(new Node(to, cost));
+			graph.get(from).add(new Node(to, cost, 0));
+			graph.get(to).add(new Node(from, cost, 0));
 		}
 		
 		System.out.println(solve(A, B, C));
@@ -51,28 +54,28 @@ public class Main {
 	
 	public static int solve(int start, int end, int goal) {
 		int[] dist = new int[N+1];
-		int max = Integer.MIN_VALUE;
+		int max = -1;
 		
 		PriorityQueue<Node> queue = new PriorityQueue<Node>();
-		queue.add(new Node(start, 0));
+		queue.add(new Node(start, 0, 0));
 		
 		while(!queue.isEmpty()) {
 			Node tmp = queue.poll();
 			
-			if(tmp.idx == end && tmp.cost <= goal) {
-				return max;
+			if(tmp.idx == end && tmp.cost <= goal && tmp.cost != 0) {
+				max = Math.max(max, tmp.max);
 			}
 			
 			for(Node next: graph.get(tmp.idx)) {
 				if(next.cost + tmp.cost <= goal) {
-					if(dist[next.idx] < next.cost + tmp.cost) {
+					if(dist[next.idx] <= next.cost + tmp.cost) {
 						dist[next.idx] = next.cost + tmp.cost;
-						queue.add(new Node(next.idx, dist[next.idx]));
-						max = Math.max(max, next.cost);
+						queue.add(new Node(next.idx, dist[next.idx], Math.max(tmp.max, next.cost)));
 					}
 				}
 			}
 		}
-		return -1;
+		return max;
+		
 	}
 }
