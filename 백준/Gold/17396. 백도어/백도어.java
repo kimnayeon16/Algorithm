2effdca21dbe8b5e100c6fd9bbf931e1158d2026
Kim Nayeon <87288958+kimnayeon16@users.noreply.github.com>
2025-02-13 13:28:49 +0900

    [Gold V] Title: 백도어, Time: 1336 ms, Memory: 160584 KB -BaekjoonHub

---
+++ 백준/Gold/17396. 백도어/백도어.java
@@ -0,0 +1,98 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static class Node implements Comparable<Node>{
+		private int idx;
+		private long cost;
+		
+		public Node(int idx, long cost) {
+			this.idx = idx;
+			this.cost = cost;
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			return Long.compare(cost, o.cost);
+		}
+	}
+	
+	static int N;
+	static List<List<Node>> graph = new ArrayList<List<Node>>();
+	static int[] vision;
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		vision = new int[N];
+		
+		st = new StringTokenizer(bf.readLine());
+		for(int i=0; i<N; i++) {
+			vision[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for(int i=0; i<=N; i++) {
+			graph.add(new ArrayList<>());
+		}
+		
+		for(int i=0; i<M; i++) {
+			st = new StringTokenizer(bf.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int t = Integer.parseInt(st.nextToken());
+			
+			graph.get(a).add(new Node(b,t));
+			graph.get(b).add(new Node(a,t));
+		}
+		
+		System.out.println(solve(0));
+		
+	}
+	public static long solve(int start) {
+		long[] dist = new long[N];
+		Arrays.fill(dist, Long.MAX_VALUE);
+		dist[start] = 0;
+		
+		boolean[] visited = new boolean[N];
+		visited[0] = true;
+		for(int i=0; i<N; i++) {
+			if(vision[i] == 1) {
+				visited[i] = true;
+			}
+		}
+		visited[N-1] = false;
+		
+		PriorityQueue<Node> queue = new PriorityQueue<Node>();
+		queue.add(new Node(start, 0));
+		
+		while(!queue.isEmpty()) {
+			Node tmp = queue.poll();
+			
+			if(tmp.cost > dist[tmp.idx]) continue;
+			
+			for(Node next: graph.get(tmp.idx)) {
+				if(vision[next.idx] == 0 || next.idx == N-1) {
+					if(!visited[next.idx] && dist[next.idx] > tmp.cost + next.cost) {
+						dist[next.idx] = tmp.cost + next.cost;
+						queue.add(new Node(next.idx, dist[next.idx]));
+					}
+				}
+			}
+		}
+		
+		if(dist[N-1] == Long.MAX_VALUE) {
+			return -1;
+		}else {
+			return dist[N-1];
+		}
+	}
+}
