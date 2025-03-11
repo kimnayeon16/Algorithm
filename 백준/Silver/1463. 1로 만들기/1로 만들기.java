523f441783e3530f8770698d648d92fd7b4cccfb
Kim Nayeon <87288958+kimnayeon16@users.noreply.github.com>
2025-03-11 23:17:36 +0900

    [Silver III] Title: 1로 만들기, Time: 96 ms, Memory: 15544 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,31 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
-	static Integer[] dp;
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		
-		int X = sc.nextInt();
+		int N = Integer.parseInt(bf.readLine());
+		int[] dp = new int[N+1];
 		
-		dp = new Integer[X+1];
-		dp[0] = dp[1] = 0;
-		
-		System.out.println(recur(X));
-	}
-
-	private static int recur(int x) {
-		if(dp[x] == null) {
-			if(x % 6 == 0) {
-				dp[x] = Math.min(recur(x-1), Math.min(recur(x/3), recur(x/2)))+1;
-			}else if(x % 3 == 0) {
-				dp[x] = Math.min(recur(x/3), recur(x-1))+1;
-			}else if(x % 2 == 0) {
-				dp[x] = Math.min(recur(x/2), recur(x-1))+1;
-			}else {
-				dp[x] = recur(x-1)+1;
+		for(int i=2; i<=N; i++) {
+			
+			dp[i] = dp[i-1] + 1;
+			
+			if(i % 2 == 0) {
+				dp[i] = Math.min(dp[i], dp[i/2]+1);
+			}
+			if(i % 3 == 0) {
+				dp[i] = Math.min(dp[i], dp[i/3]+1);
 			}
 		}
-		return dp[x];
+		System.out.println(dp[N]);
 	}
 }
