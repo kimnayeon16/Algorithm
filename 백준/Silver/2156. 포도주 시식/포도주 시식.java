55b1ff30e3ddc48d18c57314af9c915a70ebe532
Kim Nayeon <87288958+kimnayeon16@users.noreply.github.com>
2025-03-12 00:14:18 +0900

    [Silver I] Title: 포도주 시식, Time: 84 ms, Memory: 13116 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -10,21 +10,20 @@ public class Main {
 		
 		int n = Integer.parseInt(bf.readLine());
 		
-		int[] arr = new int[10001];
-		int[] dp = new int[10001];
+		int[] arr = new int[n+1];
+		int[] dp = new int[n+1];
 		
 		for(int i=1; i<=n; i++) {
 			arr[i] = Integer.parseInt(bf.readLine());
 		}
 			
 		dp[1] = arr[1];
-		dp[2] = arr[1] + arr[2];
-		int ans = Math.max(dp[1], dp[2]);
+		if(n >= 2) {
+			dp[2] = arr[1] + arr[2];			
+		}
 		
 		for(int i=3; i<=n; i++) {
 			dp[i] = Math.max(dp[i-1], Math.max(arr[i-1] + dp[i-3] , dp[i-2]) + arr[i]);
-			
-			ans = Math.max(ans, dp[i]);
 		}
 		System.out.println(dp[n]);
 	}
