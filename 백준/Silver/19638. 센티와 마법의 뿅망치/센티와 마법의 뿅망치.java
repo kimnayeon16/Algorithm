cbf9b57babe2c212dcecbcdfeb6250f711dcd5cb
Kim Nayeon <87288958+kimnayeon16@users.noreply.github.com>
2025-03-13 12:20:14 +0900

    [Silver I] Title: 센티와 마법의 뿅망치, Time: 392 ms, Memory: 31376 KB -BaekjoonHub

---
+++ 백준/Silver/19638. 센티와 마법의 뿅망치/센티와 마법의 뿅망치.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		StringBuilder sb = new StringBuilder();
+		PriorityQueue<Integer> queue = new PriorityQueue<Integer>((o1, o2) -> o2 - o1);
+		
+		int N = Integer.parseInt(st.nextToken());
+		int H = Integer.parseInt(st.nextToken());
+		int T = Integer.parseInt(st.nextToken());
+		
+		for(int i=0; i<N; i++) {
+			queue.add(Integer.parseInt(bf.readLine()));
+		}
+		
+		for(int i=0; i<T; i++) {
+			if(queue.peek() < H) {
+				sb.append("YES").append("\n").append(i);
+				System.out.println(sb);
+				return;
+			}
+			
+			int tmp = queue.poll()/2;
+			if(tmp == 0) {
+				queue.add(1);
+			}else {
+				queue.add(tmp);
+			}
+		}
+		
+		if(queue.peek() < H) {
+			sb.append("YES").append("\n").append(T);
+		}else {
+			sb.append("NO").append("\n").append(queue.peek());
+		}
+		System.out.println(sb);
+		
+	}
+}
