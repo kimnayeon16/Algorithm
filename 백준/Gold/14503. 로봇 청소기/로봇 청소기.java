3188c315556e69cc47d14cfc75e4baf67853fc10
Kim Nayeon <87288958+kimnayeon16@users.noreply.github.com>
2025-04-11 11:00:36 +0900

    [Gold V] Title: 로봇 청소기, Time: 72 ms, Memory: 11848 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -1,66 +1,69 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int[] dr = {-1,0,1,0}; //상우하좌(북동남서)
-	static int[] dc = {0,1,0,-1};
-	static int[][] arr;
-	static int N; static int M;
-	static int count = 0;
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		N = sc.nextInt();
-		M = sc.nextInt();
-		int x = sc.nextInt();
-		int y = sc.nextInt();
-		int d = sc.nextInt();
-		arr = new int[N][M];
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<M; j++) {
-				arr[i][j] = sc.nextInt();
-			}
-		}
-
-		dfs(x,y,d);
-		System.out.println(count);
-	}
-	private static void dfs(int r, int c, int d) {
-		//현재 위치 청소
-		if(arr[r][c] == 0) {
-			arr[r][c] =2;
-			count++;
-		}
-		
-		
-		boolean flag = false;
-		int origin = d;
-		
-		for(int i=0; i<4; i++) {
-			int direction = (d+3)%4;
-			int nr = r + dr[direction];
-			int nc = c + dc[direction];
-			
-			if(0<=nr && nr<N && 0<=nc && nc<M && arr[nr][nc]==0) {
-				dfs(nr, nc, direction);
-				flag = true;
-				break;
-			}
-			d = (d +3)%4;
-		}
-		
-		//네 방향 모두 청소가 되어있거나 벽인 경우
-		if(!flag) {
-			int back = (origin+2)%4;
-			int bx = r + dr[back];
-			int by = c + dc[back];
-			
-			if(0<=bx&& bx<N && 0<=by && by<M && arr[bx][by] != 1) {
-				dfs(bx, by, origin);
-			}
-		}
-		
-		
-	}
-
 	
+	static int N;
+	static int M;
+	static int[][] arr;
+	
+	static int[] dx = {-1,0,1,0};
+	static int[] dy = {0,1,0,-1};
+	
+	static int ans = 0;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N][M];
+		
+		st = new StringTokenizer(bf.readLine());
+		int r = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		int d = Integer.parseInt(st.nextToken());
+		
+		for(int n=0; n<N; n++) {
+			st = new StringTokenizer(bf.readLine());
+			for(int m=0; m<M; m++) {
+				arr[n][m] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		cleaning(r,c,d);
+		System.out.println(ans);
+	}
+	
+	public static void cleaning(int r, int c, int a) {
+//		1. 현재 칸이 아직 청소되지 않은 경우, 청소하기
+		if(arr[r][c] == 0) {
+			arr[r][c] = 2;
+			ans++;
+		}
+		
+//		3. 현재 칸의 주변 4칸 중 청소되지 않은 빈칸이 있는 경우
+		for(int d=0; d<4; d++) {
+			a = (a+3) % 4;
+			int nx = dx[a] + r;
+			int ny = dy[a] + c;
+			
+			if(0<=nx && nx<N && 0<=ny && ny<M && arr[nx][ny] == 0) {
+				cleaning(nx,ny,a);
+				return;		
+			}
+		}
+		
+//		2. 현재 칸의 주변 4칸 중 청소되지 않은 빈칸이 없는 경우
+		int back = (a + 2) % 4;
+		int p = r + dx[back];
+		int q = c + dy[back];
+		
+		if(0<=p && p<N && 0<=q && q<M && arr[p][q] != 1) {
+			cleaning(p,q,a);
+		}
+	}
 }
