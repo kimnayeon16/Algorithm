903927b8bdffda8fbf58b6470c3ee3c3bdcca1c2
Kim Nayeon <87288958+kimnayeon16@users.noreply.github.com>
2025-06-19 10:53:26 +0900

    [Silver II] Title: 연속합, Time: 164 ms, Memory: 22176 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -1,14 +1,19 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 		
-		int N = sc.nextInt();
+		int N = Integer.parseInt(bf.readLine());
 		int[] arr = new int[N];
+		
+		st = new StringTokenizer(bf.readLine());
 		for(int i=0; i<N; i++) {
-			arr[i] = sc.nextInt();
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		int[] dp = new int[N];
 		dp[0] = arr[0];
