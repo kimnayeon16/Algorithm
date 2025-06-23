1f4dea501589454fe8787624e1ea1944afd4c8da
Kim Nayeon <87288958+kimnayeon16@users.noreply.github.com>
2025-06-23 11:44:32 +0900

    [Silver IV] Title: 설탕 배달, Time: 68 ms, Memory: 11568 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,28 +1,29 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	static int min;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		
-		int N = sc.nextInt();
-		
-		//5의 배수로 나누어떨어지는가?
-		A : if(N % 5 == 0) {
-			System.out.println(N/5);
-		}else if(N % 5 != 0) {
-			int count = 0;
-			for(int i=N-3; i>0; i-=3) {
-				count++;
-				if(i % 5 == 0) {
-					System.out.println(i/5 + count);
-					break A;
-				}
-			}
-			if(N % 3 == 0) {
-				System.out.println(N/3);
-			}else {
-				System.out.println(-1);
-			}
+		//설탕 입력
+		int N = Integer.parseInt(bf.readLine());
+		//설탕 Nkg을 구하기 위해 하나 더 큰 배열 생성
+		int[] dp = new int[N+1];
+		for(int i=1; i<=N; i++) {
+			//최소의 개수를 구해야하기 때문에 최소값을 큰 값으로 임의 설정
+			min = 987654321;
+			if(i>=3) min = Math.min(min, dp[i-3]+1);
+			if(i>=5) min = Math.min(min, dp[i-5]+1);
+            
+			dp[i]=min;
+		}
+		if(min == 987654321) {
+			System.out.println(-1);
+		}else {
+			System.out.println(dp[N]);
 		}
 	}
 }
